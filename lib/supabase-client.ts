490ee07867fc8726002d0aef7b605291fd9f9ff0
alexcartn <carton51@hotmail.fr>
2025-03-18 11:54:17 +0100

    Update supabase-client.ts

--- lib/supabase-client.ts
+++ lib/supabase-client.ts
@@ -1,6 +1,10 @@
-import { createBrowserClient as createClient } from "@supabase/ssr"
 
-export function createBrowserClient() {
-  return createClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!)
-}
+import { createBrowserClient } from "@supabase/ssr";
+
+export const createClient = () =>
+  createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  );
+
 
