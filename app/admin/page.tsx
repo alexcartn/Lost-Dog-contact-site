5448718f1358742aeb231e93e9f09641982abaf7
alexcartn <carton51@hotmail.fr>
2025-03-18 15:49:03 +0100

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 import { createServerSupabaseClient } from "@/lib/supabase-server";
 
 export default async function AdminPage() {
-  const supabase = supabaseServer();
+  const supabase = createServerSupabaseClient();
   const { data: user, error } = await supabase.auth.getUser();
 
   if (error) return <div>Erreur: {error.message}</div>;
